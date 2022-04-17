73ba73822bfcbc11ab23150de4fb12f3517f1646
Axel Aquino <aquino.axel@gmail.com>
2022-04-17 00:21:27 -0400

    authenticating user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,13 @@ class UsersController < ApplicationController
 
   # GET /users/1
   def show
-    render json: @user
+    @user = User.find_by(id: session[:user_id])
+    
+    if @user
+      render json: @user
+    else
+      render json: {error: "not authorized"}, status: :unauthorized
+    end
   end
 
   # POST /users
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,17 @@ Rails.application.routes.draw do
   resources :users
   resources :beats
   resources :sounds
+
+  # route for creating user login
+  post "/login", to: "sessions#create"
+
+  # route for logining out 
+  delete "/logout", to: "sessions#destroy"
+
+  # route to stay logged in 
+
+  get "/me", to: "users#show"
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
