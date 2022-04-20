992af9c9a30f1171bad910be81b0244ebb5aceb8
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2022-04-20 12:19:25 -0400

    add kit

---
+++ client/src/AddKit.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function AddKit() {
+    const [formData, setFormData] = useState({
+        name: ""
+    })
+
+     
+    return (
+        <div>AddKit</div>
+    )
+}
+
+export default AddKit
